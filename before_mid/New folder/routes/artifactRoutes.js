f10f99ef4091f88c363d6127e73944c4b0e69491
muntahaaa <bsse1450@iit.du.ac.bd>
2024-11-18 00:55:13 -0800

    before mid added

--- before_mid/New folder/routes/artifactRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { createArtifact, uploadCSV } = require('../controllers/artifactController');
-
-const router = express.Router();
-router.post('/create', createArtifact);
-router.post('/upload-csv', uploadCSV);
-
-module.exports = router;
--- before_mid/New folder/routes/senderRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { createSender } = require('../controllers/senderController');
-
-const router = express.Router();
-router.post('/create', createSender);
-
-module.exports = router;
