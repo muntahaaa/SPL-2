3a290ce57a6db8b2af89762b627db43fd590f134
Sumaiya Farhana Islam Muntaha <bsse1450@iit.du.ac.bd>
2024-11-18 00:20:56 -0800

    Delete New folder directory

--- New folder/database/migrations/drop.sql
+++
@@ -1,7 +0,0 @@
--- psql -U postgres -f models/drop.sql
--- Terminate all connections to the database
-SELECT pg_terminate_backend(pid)
-FROM pg_stat_activity
-WHERE datname = 'file_integration';
-DROP DATABASE file_integration;
-DROP ROLE file_user;
